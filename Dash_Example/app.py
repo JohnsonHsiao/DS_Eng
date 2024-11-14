b9231cd6023748af6b253eb3372774da3f89bed4
Johnson <109583880+JohnsonHsiao@users.noreply.github.com>
2024-11-14 02:02:22 -0800

    Create app.py

---
+++ Dash_Example/app.py
@@ -0,0 +1,9 @@
+import dash
+from dash import html
+
+app = dash.Dash(__name__)
+app.layout = html.Div("Hello, World!")
+server = app.server  # 用于 gunicorn
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
