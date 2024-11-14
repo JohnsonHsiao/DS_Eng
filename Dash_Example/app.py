b741b467eef1531b4b6386d1c01b407aa530df3c
Johnson <109583880+JohnsonHsiao@users.noreply.github.com>
2024-11-14 02:14:20 -0800

    Update app.py

--- Dash_Example/app.py
+++ Dash_Example/app.py
@@ -6,4 +6,5 @@ app.layout = html.Div("Hello, World!")
 server = app.server  # 用于 gunicorn
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(host="0.0.0.0", port=8080)
+
